fbf873a81b6d49526e5d236b436722f91919c1b8
krishna1rpr <krishna1rpr@gmail.com>
2022-12-13 16:19:56 +0530

    added screen folder with login screen

---
+++ lib/screens/login.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final Map<String, TextEditingController> loginController = {
+    'userName': TextEditingController(),
+    'password': TextEditingController()
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: _formKey,
+          child: Column(children: [
+            TextFormField(
+              controller: loginController['username'],
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter some value";
+                }
+              },
+            ),
+            TextFormField(
+              controller: loginController['password'],
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter some value";
+                }
+              },
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
