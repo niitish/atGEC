630c2ad3ec9493ecafe7ce63554c89e20c00167d
krishna1rpr <krishna1rpr@gmail.com>
2022-12-15 19:20:38 +0530

    added popup

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,4 +1,6 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,12 +13,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        const Text("Home Page"),
-        BottomNavigationBar(items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home))
-        ])
-      ]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text("Home Page"),
+          Container(
+            child: RandomGenerator(),
+            margin: EdgeInsets.symmetric(vertical: 100),
+          ),
+          const BottomBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(items: const <BottomNavigationBarItem>[
+      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+    ]);
+  }
+}
+
+class RandomGenerator extends StatelessWidget {
+  RandomGenerator({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextButton(
+            child: const Text("Click to Generate"),
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => _PopUp(context));
+            })
+      ],
+    );
+  }
+
+  int generateNum() {
+    var randomNum = Random();
+    int num = randomNum.nextInt(10000);
+    var url = Uri.https('localhost', ':3000/test');
+    var response = http.post(url, body: {"value": num.toString()});
+
+    return num;
+  }
+
+  Widget _PopUp(BuildContext context) {
+    return AlertDialog(
+      title: const Text("OTP for current class:"),
+      content: Column(children: [Text(generateNum().toString())]),
+      actions: [
+        TextButton(
+          child: const Text("Close"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        )
+      ],
     );
   }
 }
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -37,7 +35,7 @@ class _LoginPageState extends State<LoginPage> {
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return "Please enter some value";
-                }
+                } else {}
               },
             ),
           ]),
